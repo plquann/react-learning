ec11136009f2767fc6d50de3a4846e27d6541f1a
QuanKhs <quanphamluong@gmail.com>
2021-02-05 19:06:37 +0700

    02-stateful-commponents

--- src/App.js
+++ src/App.js
@@ -3,14 +3,23 @@ import './App.css';
 import HeaderFunc from './components/HeaderFunc';
 import HeaderClass from './components/HeaderClass';
 import LayoutExercise from './components/LayoutExercise/LayoutExercise';
-import DataBinding from './DataBinding/DataBinding';
-
+import DataBinding from './components/DataBinding/DataBinding';
+import HandleEvent from './components/HandleEvent/HandleEvent';
+import DemoState from './components/StateReact/DemoState';
+import ChooseCar from './components/StateReact/ChooseCar';
+// import RenderWithMap from './RenderWithMap/RenderWithMap';
 
 function App() {
   return (
     <div className="App">
+      {/* <HeaderFunc /> */}
+      {/* <HeaderClass /> */}
       {/* <LayoutExercise /> */}
-      <DataBinding />
+      {/* <DataBinding /> */}
+      {/* <HandleEvent /> */}
+      {/* <DemoState /> */}
+      {/* <ChooseCar /> */}
+      {/* <RenderWithMap /> */}
 
     </div>
   );
